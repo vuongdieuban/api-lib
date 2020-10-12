acffd6b3b7ac4477320ac818835e268cfa7b93fb
ban vuong <vuongdieuban@gmail.com>
2020-10-11 18:44:00 -0600

    change url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-import { HttpService } from "../services/http.service";
+import { HttpService } from '../services/http.service';
 
 export enum OperationMode {
   DEV = 'development',
@@ -8,11 +8,11 @@ export enum OperationMode {
 export class ApiConfig {
   private static BASE_URL: string;
   private static operationMode: OperationMode;
-  private static httpService = HttpService
+  private static httpService = HttpService;
 
   static setOperationMode(mode: OperationMode): void {
     this.operationMode = mode;
-    this.BASE_URL = mode === OperationMode.DEV ? 'http://api.jobs-tracker.localhost' : 'http://prod_url';
+    this.BASE_URL = mode === OperationMode.DEV ? 'https://api.jobs-tracker.localhost' : 'http://prod_url';
   }
 
   static getBaseURL(): string {
@@ -22,7 +22,7 @@ export class ApiConfig {
     return this.BASE_URL;
   }
 
-  static setAuthHeader(accessToken:string):void {
-    this.httpService.setAuthHeader(accessToken)
+  static setAuthHeader(accessToken: string): void {
+    this.httpService.setAuthHeader(accessToken);
   }
 }
