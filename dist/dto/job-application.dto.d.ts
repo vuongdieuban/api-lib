dc0e4efc4eb5de49e15ed16d1849e46237cab329
ban vuong <vuongdieuban@gmail.com>
2020-10-17 16:42:57 -0600

    build

--- dist/dto/job-application.dto.d.ts
+++ dist/dto/job-application.dto.d.ts
@@ -6,7 +6,7 @@ export interface JobApplicationReorderRequest {
 }
 export interface ApplicationUpdatedResponse {
     id: string;
-    statusDisplayPosition: number;
+    position: number;
     statusId: string;
     jobPostId: string;
     userId: string;
@@ -18,7 +18,7 @@ export interface JobApplicationCreateRequest {
 }
 export interface JobApplication {
     id: string;
-    statusDisplayPosition: number;
+    position: number;
     status: JobApplicationStatus;
     jobPost: JobPost;
     user: {
